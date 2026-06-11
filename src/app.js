1b5c83769ce14d3cea6dbe599623ebb531ee28c2
AryanPatel2106 <aryanpatel8082@gmail.com>
2026-06-11 15:25:03 +0530

    adding new backend and frontend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express()
 
 
@@ -30,6 +32,17 @@ import fileRouter from "./routes/file.routes.js"
 app.use("/api/v1/healthcheck",healthCheckRouter)
 app.use("/api/v1/auth",authRouter)
 app.use('/api/v1/files', fileRouter)
+
+
+
+
+// ----------------------------------
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../auth-frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../auth-frontend/dist/index.html'));
+});
 
 
 export default app
